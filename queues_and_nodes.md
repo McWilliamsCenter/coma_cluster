baaffc6e25c61f1e8322855d944f409f474b4a0c
Duncan Campbell <duncan.campbell@yale.edu>
2019-08-14 14:37:21 -0400

    added ques and node page

--- queues_and_nodes.md
+++ queues_and_nodes.md
@@ -1,2 +1,98 @@
-# Queues and Nodes
+# Queues & Nodes 
 
+
+This page contains information on the current queues and nodes installed on the Coma cluster. 
+
+This page was last populated on: 8/14/2019
+
+## Queues 
+
+
+Below is a list of the current queues Coma, including the number of nodes,             and the time limit for jobs. 
+
+
+Queue | Number of Nodes | Time Limit [days-hours:minutes:seconds] 
+----- | --------------- | --------------------------------------- 
+DEBUG | 0 | infinite 
+bigmem | 2 | 7-00:00:00 
+long | 13 | 7-00:00:00 
+short | 16 | 8:00:00 
+
+
+## Nodes 
+
+
+Below is a list of the current nodes installed on Coma, including the number of processors,             amount of memory, CPU architecture, and queue(s) to which they belong. 
+More information on a specific node can be retrieved with the following command: 
+
+```console 
+user@local:~$ scontrol show node [NODE_NAME] 
+``` 
+
+
+Node | Number of Processors | Total Memory | CPU Architecture | Queues 
+---- | -------------------- | ------------ | ---------------- | ------ 
+compute-1-1 | 64 | 512 | AMD | long, bigmem 
+compute-1-11-1 | 20 | 128 | Intel | short, long 
+compute-1-11-2 | 20 | 128 | Intel | short, long 
+compute-1-11-3 | 20 | 128 | Intel | short, long 
+compute-1-11-4 | 20 | 128 | Intel | short, long 
+compute-1-13-1 | 20 | 128 | Intel | short, long 
+compute-1-13-2 | 20 | 128 | Intel | short, long 
+compute-1-13-3 | 20 | 128 | Intel | short, long 
+compute-1-13-4 | 20 | 128 | Intel | short, long 
+compute-1-15-1 | 20 | 128 | Intel | short, long 
+compute-1-15-2 | 20 | 128 | Intel | short, long 
+compute-1-15-3 | 20 | 128 | Intel | short, long 
+compute-1-15-4 | 20 | 128 | Intel | short, long 
+compute-1-17 | 16 | 128 | Intel | short 
+compute-1-18 | 16 | 128 | Intel | short 
+compute-1-19 | 16 | 128 | Intel | short, long 
+compute-1-20 | 16 | 128 | Intel | short, long 
+compute-1-21 | 16 | 128 | Intel | short, long 
+compute-1-22 | 16 | 128 | Intel | short, long 
+compute-1-23 | 16 | 128 | Intel | short, long 
+compute-1-24 | 20 | 128 | Intel | short, long 
+compute-1-25 | 20 | 128 | Intel | short, long 
+compute-1-26 | 20 | 128 | Intel | short, long 
+compute-1-27 | 20 | 128 | Intel | short, long 
+compute-1-28 | 20 | 128 | Intel | short, long 
+compute-1-29 | 20 | 128 | Intel | short, long 
+compute-1-30 | 20 | 128 | Intel | short, long 
+compute-1-31 | 20 | 128 | Intel | short, long 
+compute-1-32 | 20 | 128 | Intel | short 
+compute-1-33 | 20 | 0 | Intel | none 
+compute-1-5-1 | 16 | 128 | Intel | short, long 
+compute-1-5-2 | 16 | 128 | Intel | short, long 
+compute-1-5-3 | 16 | 128 | Intel | short, long 
+compute-1-5-4 | 16 | 128 | Intel | short, long 
+compute-1-7-1 | 16 | 128 | Intel | short, long 
+compute-1-7-2 | 16 | 128 | Intel | short, long 
+compute-1-7-3 | 16 | 128 | Intel | short, long 
+compute-1-7-4 | 16 | 128 | Intel | short, long 
+compute-1-9-1 | 20 | 128 | Intel | short, long 
+compute-1-9-2 | 20 | 128 | Intel | short, long 
+compute-1-9-3 | 20 | 128 | Intel | short, long 
+compute-1-9-4 | 20 | 128 | Intel | short, long 
+compute-2-1 | 32 | 1024 | Intel | long, bigmem 
+compute-2-10 | 16 | 32 | AMD | short, long 
+compute-2-12 | 16 | 32 | AMD | short, long 
+compute-2-14 | 16 | 32 | AMD | short, long 
+compute-2-16 | 16 | 32 | AMD | short, long 
+compute-2-18 | 16 | 32 | AMD | short, long 
+compute-2-20 | 16 | 32 | AMD | short, long 
+compute-2-22 | 16 | 32 | AMD | short, long 
+compute-2-24 | 16 | 32 | AMD | short, long 
+compute-2-26 | 16 | 32 | AMD | short, long 
+compute-2-28 | 16 | 32 | AMD | short, long 
+compute-2-30 | 16 | 32 | AMD | short, long 
+compute-2-32 | 16 | 32 | AMD | short, long 
+compute-2-34 | 16 | 32 | AMD | short, long 
+compute-2-36 | 16 | 32 | AMD | short, long 
+compute-2-38 | 16 | 32 | AMD | short, long 
+compute-2-40 | 16 | 32 | AMD | short, long 
+compute-2-6 | 16 | 32 | AMD | short, long 
+compute-2-8 | 16 | 32 | AMD | short, long 
+
+
+This page was created by running the `queues_and_nodes.py` script on Coma. 
--- queues_and_nodes.py
+++ queues_and_nodes.py
@@ -2,58 +2,99 @@
 """
 
 from __future__ import print_function
-import subprocess
-import re
+from slurm_utils import get_nodes_dict, get_partitions_dict
+import numpy as np
+import datetime
 
 def main():
 
-    # read in 'sinfo output'
-    result = subprocess.check_output(['sinfo'])
-    lines = result.split('\n')
-    colnames = lines[0].split()
+    node_dict = get_nodes_dict()
+    nodes = node_dict.keys()
+    nodes.sort()
 
-    partition_dict = {}
-    for line in lines[1:-1]:
-        partition = line.split()[0].strip('*')
-        avail = line.split()[1]
-        time_limit = line.split()[2]
-        num_nodes = int(line.split()[3])
-        state = line.split()[4].strip('*')
-        partition_dict[partition] = {'AVAIL': avail,
-                                     'TIMELIMIT': time_limit,
-                                     'NODES': num_nodes,
-                                     'STATE': state,
-                                     'NODELIST': []}
+    partition_dict = get_partitions_dict()
+    partitions = partition_dict.keys()
+    partitions.sort()
 
-    result = subprocess.check_output(['sinfo', '-N']).strip()
-    lines = result.split('\n')
+    output_file  = open('queues_and_nodes.md', 'w')
 
-    node_dict = {}
-    for line in lines[1:-1]:
-        node_name = line.split()[0]
-        partition = line.split()[2].strip('*')
-        state = line.split()[3].strip('*')
-        node_dict[node_name] = {'PARTITION': partition, 'STATE': state}
-        partition_dict[partition]['NODELIST'].append(node_name)
+    output_file.write("# Queues & Nodes \n")
+    output_file.write("\n")
+    output_file.write("\n")
 
-    result = subprocess.check_output(['scontrol', 'show', 'node']).strip()
-    # node_entries = result.split('\n\n')
-    node_entries = re.split(r'\n|,', result)
+    line = ("This page contains information on the current queues and nodes installed on the Coma cluster. \n")
+    output_file.write(line)
+    output_file.write("\n")
 
-    for node_entry in node_entries:
-        node_entry = re.split(r'\n|\s+', node_entry)
-        node_entry = [x for x in node_entry if x != '']
+    now = datetime.datetime.now()
+    date = str(now.month) + '/' + str(now.day) + '/' + str(now.year)
+
+    line = ("This page was last populated on: " + date )
+    output_file.write(line)
+    output_file.write("\n")
+    output_file.write("\n")
+
+    output_file.write("## Queues \n")
+    output_file.write("\n")
+    output_file.write("\n")
+
+    line = ("Below is a list of the current queues Coma, including the number of nodes,\
+             and the time limit for jobs. \n")
+    output_file.write(line)
+    output_file.write("\n")
+    output_file.write("\n")
+
+    output_file.write("Queue | Number of Nodes | Time Limit [days-hours:minutes:seconds] \n")
+    output_file.write("----- | --------------- | --------------------------------------- \n")
+    for queue in partitions:
+        n_nodes = partition_dict[queue]['NODES']
+        time_limit = partition_dict[queue]['TIMELIMIT']
+        line = " ".join([queue, "|", str(n_nodes), "|", time_limit, "\n"])
+        output_file.write(line)
+    output_file.write("\n")
+    output_file.write("\n")
+
+    output_file.write("## Nodes \n")
+    output_file.write("\n")
+    output_file.write("\n")
+
+    line = ("Below is a list of the current nodes installed on Coma, including the number of processors,\
+             amount of memory, CPU architecture, and queue(s) to which they belong. \n")
+    output_file.write(line)
+    line = ("More information on a specific node can be retrieved with the following command: \n")
+    output_file.write(line)
+    output_file.write("\n")
+    output_file.write("```console \n")
+    output_file.write("user@local:~$ scontrol show node [NODE_NAME] \n")
+    output_file.write("``` \n")
+    output_file.write("\n")
+    output_file.write("\n")
+
+    output_file.write("Node | Number of Processors | Total Memory | CPU Architecture | Queues \n")
+    output_file.write("---- | -------------------- | ------------ | ---------------- | ------ \n")
+    for node in nodes:
+        n = int(np.ceil(np.log10(int(node_dict[node]['RealMemory']))/np.log10(2)))
+        mem = (2**n)/1024
+        if 'amd' in node_dict[node]['AvailableFeatures']:
+            cpu = 'AMD'
+        elif 'intel' in node_dict[node]['AvailableFeatures'][0]:
+            cpu = 'Intel'
+        else:
+            print(node_dict[node]['AvailableFeatures'])
+            cpu = 'unknown'
         try:
-            node_name = node_entry[0].split('=')[0]
-            for prop in node_entry[1:]:
-                try:
-                    key, value = prop.split('=')
-                    print(node_name, key, value)
-                    node_dict[node_name][key] = value
-                except ValueError:
-                    print('     ', prop)
-        except IndexError:
-            print(node_entry)
+            queues = ', '.join(node_dict[node]['Partitions'])
+        except KeyError:
+            queues = 'none'
+
+        line = " ".join([node, "|", node_dict[node]['CPUTot'], "|", str(mem), "|", cpu, "|", queues, "\n"])
+        output_file.write(line)
+
+    output_file.write("\n")
+    output_file.write("\n")
+    line = ("This page was created by running the `queues_and_nodes.py` script on Coma. \n")
+    output_file.write(line)
+
 
 if __name__ == "__main__":
     main()
